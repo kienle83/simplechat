9a757e83e45dc6581be2567abdd44ac80ada83c1
kienle <kienle83@gmail.com>
2017-03-20 02:33:26 +0100

    some idea for unit test

--- test/test.js
+++ test/test.js
@@ -7,3 +7,25 @@ describe("true", function() {
         expect(true).toBeTruthy();
     });
 });
+
+describe('ChatControllerTest', function() {
+
+    beforeEach(angular.mock.module('app'));
+
+    var $controller;
+
+    beforeEach(angular.mock.inject(function(_$controller_) {
+        $controller = _$controller_;
+    }));
+
+    describe('type on message input', function() {
+        it('test should show text is typing if user type on message input', function() {
+            // some codes here
+        });
+
+        it('test should show show message in container after user submit', function() {
+            // for testing
+        });
+    });
+
+});
